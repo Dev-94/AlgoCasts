c22779582ebece410d974b4c0920e3e188e802d4
Dev Govindji <47125945+Dev-94@users.noreply.github.com>
2020-03-03 14:57:24 -0600

    solves capitalize in another way

--- capitalize/index.js
+++ capitalize/index.js
@@ -8,50 +8,52 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 function capitalize(str) {
-    // creates an empty array to store each word in str
-    let words = []
-    // splits str into words by finding spaces
-    for (let word of str.split(' ')) {
-        // capitalizes first letter of each word
-        // takes rest of each word
-        // pushed them into words array
-        words.push(word[0].toUpperCase() + word.slice(1))
 
-
+    // BELOW WORKS
+    // creates a string with the first letter uppercase
+    let result = str[0].toUpperCase()
+    // loops through str
+    for (let i = 1; i < str.length; i++) {
+        // if char to the left of i is a space, capitalize letter
+        if (str[i - 1] === ' ') {
+            // add to result string
+            result += str[i].toUpperCase()
+        }
+        // add letter to string if not first letter
+        else {
+            result += str[i]
+        }
     }
-    // joins all words arrays with capitalized letters
-    return words.join(' ')
 
-    // loop through str
-    // slice each letter and if char is [0] or after ' '
-    // .toUpperCase() charAt(0)
-    // if charAt(i) === ' ' .toUpperCase()
-
-    // str.slice(0, str.length)
-
-    // str.charAt(0).toUpperCase()
+    // retrun final string
+    return result
 
 
-    // for (let i = 0; i <= str.length; i++) {
-    //     if (str.charAt(i) === ' ')
-    //         str.charAt(i + [1]).toUpperCase()
+
+
+
+
+
+
+
+    // //  BELOW WORKS
+    // // creates an empty array to store each word in str
+    // let words = []
+    // // splits str into words by finding spaces
+    // for (let word of str.split(' ')) {
+    //     // capitalizes first letter of each word
+    //     // takes rest of each word
+    //     // pushed them into words array
+    //     words.push(word[0].toUpperCase() + word.slice(1))
+
 
     // }
+    // // joins all words arrays with capitalized letters
+    // return words.join(' ')
 
-    // uppercases first letter, returns first letter
-    // let first = str.slice(0, 1).toUpperCase()
-    // console.log(first)
 
-    // // iterates through string
-    // for (let i = 0; i <= str.length; i++) {
-    //     // finds spaces
-    //     if (str.charAt(i) === ' ') {
-    //         // upper cases letters after spaces
-    //         let newStr = str.slice(i + 1, i + 2).toUpperCase()
-    //         console.log(newStr)
-    //     }
-    // }
-    // console.log(  str.slice(1).toUpperCase())
+
+
 }
 
 module.exports = capitalize;
